ee96887bccdaa22428dbf92deec32d03a5611478
Kiril Kobzar <kirilkobzar@gmail.com>
2023-03-26 08:20:48 +0000

    mongo db

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
 
 # посилання на наш сайт
 URL = "https://www.imdb.com/chart/top/"
+
+MONGO_URL = "mongodb+srv://kkobzar:12345@cluster0.k2acs6d.mongodb.net/?retryWrites=true&w=majority"
 
 # відправка зпапиту на сторінку
 data = requests.get(URL).text
@@ -10,7 +13,21 @@ data = requests.get(URL).text
 # парсимо сторінку
 soup = BeautifulSoup(data, features="html.parser")
 
-films = soup.find_all("td", {"class":"titleColumn"})
+# знаходимо назви фільмів
+films = soup.find_all("td", {"class": "titleColumn"})
 
-for f in films:
+# знаходимо рейтинг фільмів
+ratings = soup.find_all("td", {"class": "ratingColumn imdbRating"})
+
+for f in films[0:10]:
   print(f.getText())
+
+for r in ratings[0:10]:
+  print(r.getText())
+
+# підключаємось до нашої датабази
+client = MongoClient(MONGO_URL)
+
+db = client[""]
+films = db[""]
+films.insert_one({"film":"test"})
